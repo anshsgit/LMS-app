7dd4607ad684004c391b0d44663633f2b652d243
anshsgit <anshsmail738@gmail.com>
2025-12-23 09:33:08 +0530

    removed client files

--- client/src/app/store.js
+++
@@ -1,8 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit';
-import { authReducer } from '../features/authSlice';
-
-export const appStore = configureStore({
-    reducer: {
-        auth: authReducer
-    },
-});
